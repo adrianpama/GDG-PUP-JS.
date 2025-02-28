c6bc8578fb34c5a3722bbaa5a8411d48cd16860b
adrianpama <adrianppama@iskolarngbayan.pup.edu.ph>
2025-03-01 00:59:10 +0800

    Update script.js

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,9 @@
+// Step 1: Create an array named favoriteMovies
+const favoriteMovies = ["Wreck It Ralph", "A Silent Voice", "Your Name", "Tangled", "Parasite"];
+
+// Step 2: Use a loop to iterate through the favoriteMovies array
+console.log("My Favorite Movies:");
+for (let i = 0; i < favoriteMovies.length; i++) {
+    // Step 3: Log each movie title 
+    console.log(favoriteMovies[i]);
+}
