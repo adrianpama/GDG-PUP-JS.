862bfd9d5dc9775125e8d71b8561ab5f1dd0b97e
adrianpama <adrianppama@iskolarngbayan.pup.edu.ph>
2025-03-02 19:12:51 +0800

    Update JS8.js

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,17 @@
 // your code starts here
+const counterText = document.getElementById('counter-text');
+const addButton = document.getElementById('button-add');
+const subtractButton = document.getElementById('button-subtract');
+
+let counterValue = 0;
+
+addButton.addEventListener('click', function() {
+    counterValue++;
+    counterText.textContent = counterValue;
+});
+
+subtractButton.addEventListener('click', function() {
+    if (counterValue > 0) {
+        counterValue--;
+        counterText.textContent = counterValue;
+    }
